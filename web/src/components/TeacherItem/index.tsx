2f5acd9ae4aa550456196f706177ee8b8c089539
Igor Cazé Nunes <igorcaze@gmail.com>
2020-08-05 18:15:42 -0300

    Finalizado frontend web
    Finalizado terceiro dia da NLW.

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,43 +3,59 @@ import React from "react";
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
+import api from "../../services/api";
 
-function TeacherItem() {
-  return (
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+       api.post('connections', {
+           user_id: teacher.id,
+       });
+    }
+
+    return (
         <article className="teacher-item">
             <header>
                 <img
-                    src="https://avatars0.githubusercontent.com/u/30866274?s=400&u=88fdd8b1aeefaadaafd33196f451061e3e7b4574&v=4"
-                    alt="Igor Cazé"
+                    src={teacher.avatar}
+                    alt={teacher.name}
                 />
-                
+
                 <div>
-                    <strong>Igor Cazé</strong>
-                    <span>Matemática</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi nulla ea
-                quo aliquam voluptatum cupiditate fugiat, error voluptatibus blanditiis
-                quae voluptatem impedit molestias esse natus nisi quos at? Itaque,
-                laudantium?
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 1000,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
 
-                <button type="button">
+                <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} target="_blank">
 
-                <img src={whatsappIcon} alt="whatsapp" />
+                    <img src={whatsappIcon} alt="whatsapp" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
-  );
+    );
 }
 
 export default TeacherItem;
